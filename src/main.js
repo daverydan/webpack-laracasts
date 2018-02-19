48a1fdde9f29b3a89e3bb169e23c810a75aebc06
Danny Avery <danny.w.avery@gmail.com>
2018-02-19 13:44:15 -0700

    ES2015 Compilation w/ Babel

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-require('./main.css');
+class Form {
+	constructor() {
+		// alert('Babel is awesome!');
+		let numbers = [5, 10, 15].map(number => 2 * number);
+
+		console.log(numbers);
+	}
+}
+new Form();
